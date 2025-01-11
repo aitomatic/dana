e0054915fbb2f39018805d7e53a903d4758b90fd
The Vinh LUONG (LƯƠNG Thế Vinh) <TheVinhLuong@GMail.com>
2025-01-10 21:38:11 -0800

    add place-holder dxa.core.resource.file_resource.FileResource

---
+++ dxa/core/resource/file_resource.py
@@ -0,0 +1,7 @@
+from .base_resource import BaseResource
+
+from openssa import FileResource as OpenSsaFileResource
+
+
+class FileResource(BaseResource):
+    """File-stored Informational Resource."""
