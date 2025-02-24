5b4ad21d0b5327e93400b00a65461a0a4324b486
Your Name <nguyennm1024@gmail.com>
2025-02-23 23:57:16 -0800

    add critique for ProSEA

--- dxa/execution/reasoning/reasoning_executor.py
+++ dxa/execution/reasoning/reasoning_executor.py
@@ -16,6 +16,7 @@ from ..executor import Executor
 from .reasoning import Reasoning
 from ..planning.plan import Plan
 from ...common.graph import NodeType
+from ...common.utils.text_processor import TextProcessor
 
 class ReasoningStrategy(Enum):
     """Reasoning execution strategies."""
@@ -35,6 +36,7 @@ class ReasoningExecutor(Executor):
         self.graph: Optional[ExecutionGraph] = None  # Add type annotation
         self.layer = "reasoning"
         self._configure_logger()
+        self.parse_by_key = TextProcessor().parse_by_key
 
     async def execute(self, upper_graph: ExecutionGraph, context: ExecutionContext,
                       upper_signals: Optional[List[ExecutionSignal]] = None) -> List[ExecutionSignal]:
@@ -149,10 +151,10 @@ class ReasoningExecutor(Executor):
         assert context.reasoning_llm is not None
         # print("\033[91mPrev signals\033[0m", prev_signals)
         prev_step_output = ""
-        if prev_signals:
-            for signal in prev_signals:
-                if signal.type == ExecutionSignalType.DATA_RESULT:
-                    prev_step_output += signal.content["result"]["content"]
+        if len(prev_signals) > 2:
+            for i in range(2, len(prev_signals)):
+                if prev_signals[i].type == ExecutionSignalType.DATA_RESULT:
+                    prev_step_output += prev_signals[i].content["result"]["content"]
                     prev_step_output += "\n\n"
         if "step_" in node.node_id:
             prompt = f"""
@@ -170,12 +172,78 @@ class ReasoningExecutor(Executor):
             prompt = node.description.replace("<reasoning_result>", prev_step_output)
         else:
             prompt = node.description
-        response = await context.reasoning_llm.query({"prompt": prompt})
+        
+        
+        if validation_node is not None:
+            response = await self._execute_prosea_step_with_validation(node=node, context=context, validation_node=validation_node, original_problem=original_problem, prompt=prompt, prev_signals=prev_signals)
+        else:
+            response = await context.reasoning_llm.query({"prompt": prompt})
         return [ExecutionSignal(type=ExecutionSignalType.DATA_RESULT, content={
             "result": response,
             "node": node.node_id
         })]
+    
+    async def _validate_response(self, response: str, node: ExecutionNode, validation_node: ExecutionNode, context: ExecutionContext,
+                                 original_problem: Optional[str] = None, additional_prompt: Optional[str] = None,
+                                 prev_signals: Optional[List[ExecutionSignal]] = None) -> List[any]:
+        """Validate the response against the validation node."""
+        assert validation_node is not None
+        assert original_problem is not None
+        previous_step_output = ""
+        if len(prev_signals) > 2:
+            for i in range(2, len(prev_signals)):
+                if prev_signals[i].type == ExecutionSignalType.DATA_RESULT:
+                    previous_step_output += prev_signals[i].content["result"]["content"]
+                    previous_step_output += "\n\n"
+        
+        prompt = f"""
+        Initial analysis for the problem: {prev_signals[0].content["result"]["content"]}
+        This is previous steps: {previous_step_output} \n\n
+        This is the start prompt: {node.description}
+        This is the additional prompt used in previous try: {additional_prompt}
+        This is the output: {response}
+        This is the expected information from output: {validation_node.description}
 
+        First, please do reasoning and analysis on the output and expected information, then let me know if the output is matched with the expectation or not.
+        If not, please do analysis and tell me that can you solve this problem only by continuing generate new prompt for the conversation to achieve the expectation?
+        Do not assume the important if you are not sure and do not assume customized information (for example, different company will have different level ladder).
+        
+        Answer format:
+        reasoning_analysis: Your reasoning and analysis on the output and expected information. You need to critique the output based on expected information. Always make sure that the information provided in the output is correct. If not, that's a big issue. If the criteria is gathering information, but output is how to gather information, then it's not correct. If expected output is nothing special, any output should be accepted. If there are information that matched with all requirement in the answer (even though the answer also has irrelevant information), the answer is also matched.
+        is_matched: Specify if output is matched with criteria. Yes/No (just write Yes or No, no more). If the criteria is gathering information, but output is how to gather information or just give the questions or just give questions to get the information, then it's not correct and the answer is No. If expected output is nothing special, any output should be accepted and answer is Yes. f there are information that matched with all requirement in the answer (even though the answer has irrelevant information), the answer is also matched, please answer Yes.
+        reasoning_for_contradicted: If the output is not matched with the expectation, given the start prompt, please do reasoning to check if the criteria for the output has any mistake that will make the output inaccurate. Point out the mistake. If criteria is accept anything, it means no contradict.
+        is_contradicted: Specify if the criteria for the output has any mistake. Yes/No (just write Yes or No, no more). If the criteria is accept anything, it means no contradict and answer is No.
+        refined_criteria: If the criteria for the output has any mistake, then let me know the refined criteria for the output here. Just tel me new criteria, do not say anything else.
+        reasoning_analysis_solvable_by_continuing_prompt: Think carefully about if you can solve this problem by only ask a LLM and/or retrieve information from documents without interact with real world. If it's necessary to interact with real world or interact with device to get output, then you need to ask for more input from the user. If without input from user, the result will be unreliable, please ask user. If the answer can be achived by checking documents, continue prompt. If there is something need user's confirmation. You must ask user. Provide a clear analysis. Before asking user, is there anything from provided questions can replace the information you need? If require confirmation form human, you must ask user.
+        is_solvable_by_continuing_prompt: Based on your above analysis, specify if you can solve this problem only by continuing generate new prompt to ask LLM (and/or retrieve information from documents) to achieve the expectation. Yes/No (just write Yes or No, no more). Yes means you can solve this problem only by continuing generate new prompt for the conversation (with RAG) to achieve the expectation or there is no need addtional input from user to process. No means you must ask for more input from the user. If require confirmation form human, you must ask user by saying Yes.
+        
+        Do not lack any key above.
+        Do not forget the key and do not change the key format. Do not highlight the key.
+        """
+        response = await context.reasoning_llm.query({"prompt": prompt})
+        response_text = response["content"]
+        is_matched = "yes" in self.parse_by_key(response_text, "is_matched").lower()
+        is_contradicted = "yes" in self.parse_by_key(response_text, "is_contradicted").lower()
+        refined_criteria = self.parse_by_key(response_text, "refined_criteria")
+        is_solvable_by_continuing_prompt = "yes" in self.parse_by_key(response_text, "is_solvable_by_continuing_prompt").lower()
+        
+        return is_matched, is_contradicted, refined_criteria, is_solvable_by_continuing_prompt    
+        
+    async def _execute_prosea_step_with_validation(self, node: ExecutionNode, context: ExecutionContext,
+                              validation_node: Optional[ExecutionNode] = None,
+                              original_problem: Optional[str] = None, prompt: str = None,
+                              prev_signals: Optional[List[ExecutionSignal]] = None) -> List[ExecutionSignal]:
+        """Execute PROSEA reasoning with validation."""
+        assert context.reasoning_llm is not None
+        is_matched = False
+        print("Start validation")
+        while not is_matched:
+            response = await context.reasoning_llm.query({"prompt": prompt})
+            response_text = response["content"]
+            is_matched, is_contradicted, refined_criteria, is_solvable_by_continuing_prompt = await self._validate_response(response=response_text, node=node, validation_node=validation_node, context=context, original_problem=original_problem, additional_prompt=prompt, prev_signals=prev_signals)
+            print("is_matched", is_matched)
+        return response
+        
     async def _execute_direct(self, node: ExecutionNode, context: ExecutionContext) -> List[ExecutionSignal]:
         """Execute direct LLM query."""
         self.logger.info(
