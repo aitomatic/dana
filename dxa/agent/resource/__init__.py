0e16808d2cd0c113896c47733662edc1af0ce385
Christopher Nguyen <ctn@aitomatic.com>
2025-04-06 15:01:23 +0700

    Add ResourceFactory to agent module and remove obsolete factory files

--- dxa/agent/resource/__init__.py
+++ dxa/agent/resource/__init__.py
@@ -2,10 +2,12 @@
 
 from .agent_resource import AgentResource
 from .expert_resource import ExpertResource, ExpertConfig, ExpertResponse
+from .resource_factory import ResourceFactory
 
 __all__ = [
     "AgentResource",
     "ExpertResource",
     "ExpertConfig",
     "ExpertResponse",
+    "ResourceFactory",
 ] 
