7bb5c9e0d3dd92aa7e89c9f439ad09b0a4a6c9d8
Zooey Nguyen <zooeyn@gmail.com>
2025-04-25 16:35:23 +0700

    First implementation of Code Capability
    TODO:
    - Add dynamically-called debug node only IF something errors out in the code
    - Potentially separate code generation, parsing, and running as different tools for the resource

---
+++ opendxa/contrib/code_agent/main.py
@@ -0,0 +1,30 @@
+from pathlib import Path
+
+from dotenv import load_dotenv
+
+from opendxa.agent import Agent
+from opendxa.contrib.code_agent.code_resource import CodeResource
+from opendxa.contrib.sql_agent_with_dynamic_plan.execution.planning.dynamic_plan_executor import (
+    DynamicPlanExecutor,
+)
+from opendxa.execution.planning import PlanFactory
+
+load_dotenv()
+
+question = "I need a function to manually bubble sort the following list of numbers:\
+    [3, 2, 1, 4, 5, 6, 7, 0, -8, -12]."
+
+agent = (
+    Agent()
+    .with_planning(planner=DynamicPlanExecutor())
+    .with_resources({"code_resource": CodeResource()})
+)
+
+config_dir = Path(__file__).parent
+
+plan_graph = PlanFactory.create_plan_by_name(
+    "code_planning", objective=question, config_dir=config_dir
+)
+
+result = agent.run(plan_graph)
+print(result)
