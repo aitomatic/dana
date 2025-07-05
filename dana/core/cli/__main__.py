51b213eaa7ccbf7423efdad18938cd778291aa87
The Vinh LUONG (LƯƠNG Thế Vinh) <TheVinhLuong@GMail.com>
2025-07-04 19:20:29 -0700

    add dana.core.cli.__main__ and VSCode debugger runner for developer convenience

---
+++ dana/core/cli/__main__.py
@@ -0,0 +1,12 @@
+from pathlib import Path
+
+from dotenv import load_dotenv
+
+import dana
+from .dana import main
+
+# if developer puts an .env file in the OpenDXA repo root directory, load it
+DOTENV_PATH = Path(dana.__path__[0]).parent / ".env"
+load_dotenv(dotenv_path=DOTENV_PATH, verbose=True, override=True, encoding="utf-8")
+
+main()
