cbf3ac0357da956b859d7888a39aa014b127ffbe
The Vinh LUONG (LƯƠNG Thế Vinh) <TheVinhLuong@gmail.com>
2025-07-05 20:32:36 -0700

    enhance dana.core.cli.dana2.main(...) with load_dana_env(...)

--- dana/core/cli/dana2/__init__.py
+++ dana/core/cli/dana2/__init__.py
@@ -1,12 +1,11 @@
 import argparse
+from pathlib import Path
 
 from dana.core.lang.dana_sandbox import DanaSandbox
 from dana.core.lang.sandbox_context import SandboxContext
-from dana.util.env import assign_default_dana_config
+from dana.util.env import load_dana_env
 
 from .dana_input_args_parser import parse_dana_input_args
-
-assign_default_dana_config()
 
 def main():
     """
@@ -26,6 +25,8 @@ def main():
     context = SandboxContext()
     for key, value in input_dict.items():
         context.set(key, value)
+
+    load_dana_env(dot_env_file_path=Path(args.file_path).parent / '.env')
 
     # Run the Dana script with the prepared context
     result = DanaSandbox.quick_run(file_path=args.file_path, debug_mode=args.debug, context=context)
