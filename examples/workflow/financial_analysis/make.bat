f8fb2df3785fb7a6b98ec8b7a5566935a315d704
The Vinh LUONG (LƯƠNG Thế Vinh) <TheVinhLuong@gmail.com>
2025-07-24 21:07:37 -0700

    refactor examples/workflow/financial_analysis Dana scripts; remove dana2 executable

--- examples/workflow/financial_analysis/make.bat
+++ examples/workflow/financial_analysis/make.bat
@@ -9,15 +9,15 @@ IF "%TARGET%"=="test-all" GOTO test-all
 :: TESTS
 :: =====
 :test-all
-  dana2 test.na
-  dana2 test-adj-income.na
-  dana2 test-cap-intens.na
-  dana2 test-income-util-ratios.na
-  dana2 test-leverage.na
-  dana2 test-liquidity.na
-  dana2 test-margin-ratios.na
-  dana2 test-return-ratios.na
-  dana2 test-turnover-ratios.na
+  dana test.na
+  dana test-adj-income.na
+  dana test-cap-intens.na
+  dana test-income-util-ratios.na
+  dana test-leverage.na
+  dana test-liquidity.na
+  dana test-margin-ratios.na
+  dana test-return-ratios.na
+  dana test-turnover-ratios.na
   GOTO end
 
 :: END
