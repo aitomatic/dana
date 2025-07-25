6430731e8fb543376609ced417b9cbb38c366182
The Vinh LUONG (LƯƠNG Thế Vinh) <TheVinhLuong@gmail.com>
2025-07-24 21:33:37 -0700

    Consolidate all financial analysis tests into test_runner.na
    - Removed all legacy test*.na files in favor of a unified test_runner.na
    - Updated Makefile and make.bat to use only 'dana test_runner.na' for all and test targets
    - Ensured all __init__.na exports are covered in the new test runner
    - Simplified and modernized the test workflow for maintainability and clarity

--- examples/workflow/financial_analysis/make.bat
+++ examples/workflow/financial_analysis/make.bat
@@ -1,25 +1,5 @@
 @echo off
 
-:: TARGETS
-:: =======
-SET TARGET=%1
-
-IF "%TARGET%"=="test-all" GOTO test-all
-
-:: TESTS
-:: =====
-:test-all
-  dana test.na
-  dana test-adj-income.na
-  dana test-cap-intens.na
-  dana test-income-util-ratios.na
-  dana test-leverage.na
-  dana test-liquidity.na
-  dana test-margin-ratios.na
-  dana test-return-ratios.na
-  dana test-turnover-ratios.na
-  GOTO end
-
-:: END
-:: ===
-:end
+REM Run all Dana tests using the unified test runner
+:test
+  dana test_runner.na
