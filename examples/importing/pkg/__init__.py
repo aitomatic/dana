e7907868ad1831f3e56759f193aabe296e5db0eb
The Vinh LUONG (LƯƠNG Thế Vinh) <TheVinhLuong@gmail.com>
2025-08-09 15:33:57 -0700

    update examples/importing/pkg.__init__ with additional importing patterns

--- examples/importing/pkg/__init__.py
+++ examples/importing/pkg/__init__.py
@@ -8,8 +8,19 @@ from .big_submodule_with_empty_init.small_submodule import I_AM as SMALL_SUBMODU
 
 from . import big_submodule_with_nonempty_init
 from .big_submodule_with_nonempty_init import I_AM as BIG_SUBMODULE_WITH_NONEMPTY_INIT
+
 from .big_submodule_with_nonempty_init import small_submodule as small_submodule_in_big_submodule_with_nonempty_init
-from .big_submodule_with_nonempty_init.small_submodule import I_AM as SMALL_SUBMODULE_IN_BIG_SUBMODULE_WITH_NONEMPTY_INIT
+from .big_submodule_with_nonempty_init.small_submodule import (
+    I_AM as SMALL_SUBMODULE_IN_BIG_SUBMODULE_WITH_NONEMPTY_INIT,
+    util_submodule as util_submodule_imported_in_small_submodule_in_big_submodule_with_nonempty_init,
+    small_util_submodule_in_big_util_submodule as small_util_submodule_in_big_util_submodule_imported_in_small_submodule_in_big_submodule_with_nonempty_init,
+    SMALL_UTIL_SUBMODULE_IN_BIG_UTIL_SUBMODULE as SMALL_UTIL_SUBMODULE_IN_BIG_UTIL_SUBMODULE_IMPORTED_IN_SMALL_SUBMODULE_IN_BIG_SUBMODULE_WITH_NONEMPTY_INIT,
+)
+from .big_submodule_with_nonempty_init import (
+    util_submodule as util_submodule_in_big_submodule_with_nonempty_init,
+    small_util_submodule_in_big_util_submodule as small_util_submodule_in_big_util_submodule_imported_in_big_submodule_with_nonempty_init,
+    SMALL_UTIL_SUBMODULE_IN_BIG_UTIL_SUBMODULE as SMALL_UTIL_SUBMODULE_IN_BIG_UTIL_SUBMODULE_IMPORTED_IN_BIG_SUBMODULE_WITH_NONEMPTY_INIT,
+)
 
 from . import util_submodule
 from .util_submodule import small_submodule as small_util_submodule_in_util_submodule
@@ -205,6 +216,193 @@ print(f"""
 ACCESSED: {small_submodule_in_big_submodule_with_empty_init.SMALL_UTIL_SUBMODULE_IN_BIG_UTIL_SUBMODULE}
 IN: {I_AM}
 """)
+
+
+# access `big_submodule_with_nonempty_init` and its nested objects
+print(f"""
+IMPORTED: {big_submodule_with_nonempty_init}
+INTO: {I_AM}
+""")
+
+print(f"""
+ACCESSED: {big_submodule_with_nonempty_init.I_AM}
+IN: {I_AM}
+""")
+
+print(f"""
+ACCESSED: {BIG_SUBMODULE_WITH_NONEMPTY_INIT}
+IN: {I_AM}
+""")
+
+print(f"""
+ACCESSED: {big_submodule_with_nonempty_init.util_submodule}
+IN: {I_AM}
+""")
+
+print(f"""
+ACCESSED: {big_submodule_with_nonempty_init.util_submodule.small_submodule}
+IN: {I_AM}
+""")
+
+print(f"""
+ACCESSED: {big_submodule_with_nonempty_init.small_util_submodule_in_big_util_submodule}
+IN: {I_AM}
+""")
+
+print(f"""
+ACCESSED: {big_submodule_with_nonempty_init.util_submodule.small_submodule.I_AM}
+IN: {I_AM}
+""")
+
+print(f"""
+ACCESSED: {big_submodule_with_nonempty_init.small_util_submodule_in_big_util_submodule.I_AM}
+IN: {I_AM}
+""")
+
+print(f"""
+ACCESSED: {big_submodule_with_nonempty_init.SMALL_UTIL_SUBMODULE_IN_BIG_UTIL_SUBMODULE}
+IN: {I_AM}
+""")
+
+
+
+
+# ====== TEMP AREA
+
+
+print(f'
+IMPORTED: {SMALL_SUBMODULE_IN_BIG_SUBMODULE_WITH_EMPTY_INIT}
+INTO: {I_AM}
+')
+
+print(f'
+ACCESSED: {small_submodule_in_big_submodule_with_empty_init.util_submodule}
+IN: {I_AM}
+')
+
+print(f'
+ACCESSED: {small_submodule_in_big_submodule_with_empty_init.util_submodule.small_submodule}
+IN: {I_AM}
+')
+
+print(f'
+ACCESSED: {small_submodule_in_big_submodule_with_empty_init.small_util_submodule_in_big_util_submodule}
+IN: {I_AM}
+')
+
+print(f'
+ACCESSED: {small_submodule_in_big_submodule_with_empty_init.util_submodule.small_submodule.I_AM}
+IN: {I_AM}
+')
+
+print(f'
+ACCESSED: {small_submodule_in_big_submodule_with_empty_init.small_util_submodule_in_big_util_submodule.I_AM}
+IN: {I_AM}
+')
+
+print(f'
+ACCESSED: {small_submodule_in_big_submodule_with_empty_init.SMALL_UTIL_SUBMODULE_IN_BIG_UTIL_SUBMODULE}
+IN: {I_AM}
+')
+
+# =======
+
+print(f'
+ACCESSED: {big_submodule_with_nonempty_init.small_submodule}
+IN: {I_AM}
+')
+print(f'
+ACCESSED: {big_submodule_with_nonempty_init.small_submodule_in_big_submodule_with_nonempty_init}
+IN: {I_AM}
+')
+
+print(f'
+ACCESSED: {big_submodule_with_nonempty_init.small_submodule.I_AM}
+IN: {I_AM}
+')
+print(f'
+ACCESSED: {big_submodule_with_empty_init.small_submodule_in_big_submodule_with_nonempty_init.I_AM}
+IN: {I_AM}
+')
+
+print(f'
+USED: {big_submodule_with_nonempty_init.small_submodule.util_submodule}
+IN: {I_AM}
+')
+
+print(f'
+USED: {big_submodule_with_nonempty_init.small_submodule.util_submodule.small_submodule}
+IN: {I_AM}
+')
+
+print(f'
+USED: {big_submodule_with_nonempty_init.small_submodule.small_util_submodule_in_big_util_submodule}
+IN: {I_AM}
+')
+
+print(f'
+USED: {big_submodule_with_nonempty_init.small_submodule.util_submodule.small_submodule.I_AM}
+IN: {I_AM}
+')
+
+print(f'
+USED: {big_submodule_with_nonempty_init.small_submodule.small_util_submodule_in_big_util_submodule.I_AM}
+IN: {I_AM}
+')
+
+print(f'
+USED: {big_submodule_with_nonempty_init.small_submodule.SMALL_UTIL_SUBMODULE_IN_BIG_UTIL_SUBMODULE}
+IN: {I_AM}
+')
+
+
+# access `small_submodule_in_big_submodule_with_nonempty_init` and its nested objects
+print(f'
+USED: {small_submodule_in_big_submodule_with_nonempty_init}
+IN: {I_AM}
+')
+
+print(f'
+USED: {small_submodule_in_big_submodule_with_nonempty_init.I_AM}
+IN: {I_AM}
+')
+
+print(f'
+USED: {small_submodule_in_big_submodule_with_nonempty_init.util_submodule}
+IN: {I_AM}
+')
+
+print(f'
+USED: {small_submodule_in_big_submodule_with_nonempty_init.util_submodule.small_submodule}
+IN: {I_AM}
+')
+
+print(f'
+USED: {small_submodule_in_big_submodule_with_nonempty_init.small_util_submodule_in_big_util_submodule}
+IN: {I_AM}
+')
+
+print(f'
+USED: {small_submodule_in_big_submodule_with_nonempty_init.util_submodule.small_submodule.I_AM}
+IN: {I_AM}
+')
+
+print(f'
+USED: {small_submodule_in_big_submodule_with_nonempty_init.small_util_submodule_in_big_util_submodule.I_AM}
+IN: {I_AM}
+')
+
+print(f'
+USED: {small_submodule_in_big_submodule_with_nonempty_init.SMALL_UTIL_SUBMODULE_IN_BIG_UTIL_SUBMODULE}
+IN: {I_AM}
+')
+
+
+# access `SMALL_SUBMODULE_IN_BIG_SUBMODULE_WITH_NONEMPTY_INIT`
+print(f'
+IMPORTED: {SMALL_SUBMODULE_IN_BIG_SUBMODULE_WITH_NONEMPTY_INIT}
+INTO: {I_AM}
+')
 
 
 # access `util_submodule` and its nested objects
