5f3ac6d61259f052426a771bbbb6601afcafb610
The Vinh LUONG (LƯƠNG Thế Vinh) <TheVinhLuong@gmail.com>
2025-08-25 04:06:58 -0700

    revert edit

--- dana/apps/cli/__main__.py
+++ dana/apps/cli/__main__.py
@@ -52,8 +52,7 @@ import os
 from pathlib import Path
 import re
 import sys
-# from dotenv import load_dotenv
-# load_dotenv()
+
 import uvicorn
 
 # Set up compatibility layer for new dana structure
