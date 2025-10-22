e514d8fd51a28151ef98b37821b90de2b4363e62
Lam Nguyen <ngoclam9415@gmail.com>
2025-10-22 08:42:49 +0700

    Update teaching agent

--- examples/agents/teaching/resources/__init__.py
+++ examples/agents/teaching/resources/__init__.py
@@ -3,7 +3,6 @@ from .ripgrep_search_resource import RipgrepSearchResource
 from .edit_file_resource import EditFileResource
 from .create_file_resource import CreateFileResource
 from .list_dir_resource import ListDirResource
-from .file_modification_resource import FileModificationResource
 
 __all__ = [
     "ReadFileResource",
@@ -11,7 +10,6 @@ __all__ = [
     "EditFileResource",
     "CreateFileResource",
     "ListDirResource",
-    "FileModificationResource",
 ]
 
 
